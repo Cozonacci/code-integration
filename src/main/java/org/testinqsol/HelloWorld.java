abc7af27b09d04301ce0747db05bfc5effc797ef
Dumtru Chicu <dumitru.chicu@gmail.com>
2019-11-14 05:17:33 -0800

    IPSC-124: Something merged into master

--- src/main/java/org/testinqsol/HelloWorld.java
+++ src/main/java/org/testinqsol/HelloWorld.java
@@ -5,4 +5,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello1");
     }
+
+    public static void newMethod(){
+        System.out.println("master changed");
+    }
 }
