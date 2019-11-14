83b6dd8789d5cf8ad1193443b162e097ea592561
Dumtru Chicu <dumitru.chicu@gmail.com>
2019-11-14 05:13:12 -0800

    IPSC-123: Implement message validation via MQ

--- src/main/java/org/testinqsol/HelloWorld.java
+++ src/main/java/org/testinqsol/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello1");
+        System.out.println("feature 123-1");
     }
 
     public static void newMethod(){
